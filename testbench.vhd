be86b49484a917cf2bb8b1c988ea7343c0874dbb
RenanPicoli <renanps123@gmail.com>
2021-01-07 23:55:24 -0300

    added a testbench

---
+++ testbench.vhd
@@ -0,0 +1,68 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.all;
+use work.my_types.all;
+
+
+entity testbench is
+end testbench;
+
+architecture test of testbench is
+-- "Time" that will elapse between test vectors we submit to the component.
+constant TIME_DELTA : time := 44 us;
+
+--reset duration must be long enough to be perceived by the slowest clock (filter clock, both polarities)
+constant TIME_RST : time := 50 us;
+
+signal D: std_logic_vector(31 downto 0);--for register write
+signal CLK: std_logic;--for register read/write, also used to generate SCL
+signal ADDR: std_logic_vector(7 downto 0);--address offset of registers relative to peripheral base address
+signal WREN: std_logic;--enables register write
+signal RDEN: std_logic;--enables register read
+signal IACK: std_logic;--interrupt acknowledgement
+signal Q: std_logic_vector(31 downto 0);--for register read
+signal IRQ: std_logic;--interrupt request
+signal SDA: std_logic;--open drain data line
+signal SCL: std_logic;--open drain clock line
+
+begin
+
+	DUT: entity work.i2c_master
+	port map(D 		=> D,
+				CLK	=> CLK,
+				ADDR 	=> ADDR,
+				WREN	=> WREN,
+				RDEN	=>	RDEN,
+				IACK	=> IACK,
+				Q		=>	Q,
+				IRQ	=>	IRQ,
+				SDA	=>	SDA,
+				SCL	=>	SCL
+	);
+	
+	clock: process--100kHz input clock
+	begin
+		CLK <= '0';
+		wait for 5 us;
+		CLK <= '1';
+		wait for 5 us;
+	end process clock;
+	
+	wren_assign: process
+	begin
+		WREN <= '0';
+		wait for 5 us;
+		
+		WREN <= '1';
+		wait for 5 us;
+		
+		WREN <= '0';
+		wait;
+	end process wren_assign;
+	
+	D <= x"0000_0009";--1001
+	ADDR <= x"1A";
+	
+end architecture test;
