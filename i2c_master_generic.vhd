6c0e15cd714a2e83efd1ce1d46c82b7f2fb1ff8e
RenanPicoli <renanps123@gmail.com>
2021-01-08 14:24:55 -0300

    fixed SDA/SCL timing; added reset

--- i2c_master_generic.vhd
+++ i2c_master_generic.vhd
@@ -15,8 +15,9 @@ entity i2c_master_generic is
 	generic (N: natural);--number of bits in each data written/read
 	port (
 			DR: in std_logic_vector(N-1 downto 0);--to store data to be transmitted or received
-			CLK: in std_logic;--clock input, same frequency as SCL, used to generate SCL
 			ADDR: in std_logic_vector(7 downto 0);--address offset of registers relative to peripheral base address
+			CLK: in std_logic;--clock input, same frequency as SCL, used to generate SCL
+			RST: in std_logic;--reset
 			WREN: in std_logic;--enables register write
 			IACK: in std_logic;--interrupt acknowledgement
 			IRQ: out std_logic;--interrupt request
@@ -35,25 +36,29 @@ architecture structure of i2c_master_generic is
 	signal rx_rst: std_logic;--resets rx
 	
 	signal scl_en: std_logic;--enables scl to follow CLK
-	signal scl_en_set: std_logic:='0';--sets scl_en
-	signal scl_en_rst: std_logic:='0';--resets scl_en
+	signal scl_en_set: std_logic;--sets scl_en
+	signal scl_en_rst: std_logic;--resets scl_en
 	
-	signal fifo_empty: std_logic:='0';
+	signal fifo_empty: std_logic;
 	signal fifo_sda: std_logic_vector(N+1 downto 0);--one byte plus start and stop
-	signal bits_sent: natural := 0;--number of bits transmitted
+	signal bits_sent: natural;--number of bits transmitted
 begin
-	process (WREN,CLK,tx_rst)
+	process (WREN,CLK,tx_rst,RST)
 	begin
-		if (tx_rst = '1') then
+		if (RST ='1') then
+			tx_set <= '0';
+		elsif (tx_rst = '1') then
 			tx_set <= '0';
 		elsif(WREN'event and WREN='1') then--moment of start of shifting DR (transmission of address)
 			tx_set <= '1';
 		end if;
 	end process;
 	
-	process (fifo_empty,CLK,tx_set)
-	begin		
-		if(fifo_empty='1') then--moment of start of shifting DR (transmission of address)
+	process (fifo_empty,CLK,tx_set,RST)
+	begin
+		if (RST ='1') then
+			tx_rst <= '0';
+		elsif(fifo_empty='1') then--moment of start of shifting DR (transmission of address)
 			tx_rst <= '1';
 		elsif(tx_set'event and tx_set = '1') then
 			tx_rst <= '0';
@@ -62,18 +67,22 @@ begin
 	
 	tx <= tx_set and (not tx_rst);
 	
-	process(tx,rx,CLK,scl_en_rst)
+	process(tx,rx,CLK,scl_en_rst,RST)
 	begin
-		if(scl_en_rst = '1') then
+		if (RST ='1') then
+			scl_en_set <= '0';
+		elsif(scl_en_rst = '1') then
 			scl_en_set <= '0';
 		elsif ((tx ='1' or rx = '1') and falling_edge(CLK)) then
 			scl_en_set <= '1';
 		end if;
 	end process;
 	
-	process(bits_sent,CLK,scl_en_set)
+	process(bits_sent,CLK,scl_en_set,RST)
 	begin
-		if (bits_sent = N+2 and CLK = '1') then
+		if (RST ='1') then
+			scl_en_rst <= '0';
+		elsif (bits_sent = N+1 and CLK = '1') then
 			scl_en_rst <= '1';
 		elsif (rising_edge(scl_en_set)) then
 			scl_en_rst <= '0';
@@ -84,15 +93,18 @@ begin
 	SCL <= CLK when (scl_en = '1') else '1';
 	
 	--serial write on SDA bus
-	serial_w: process(tx,CLK,WREN,DR)
+	serial_w: process(tx,CLK,WREN,DR,RST)
 	begin
-		if (WREN = '1') then
+		if (RST ='1') then
+			fifo_sda <= (others => '1');
+			fifo_empty <= '0';
+			bits_sent <= 0;
+		elsif (WREN = '1') then
 			fifo_sda <= '0' & DR & '0';--start bit & DR & stop bit
 		elsif(tx='1' and falling_edge(CLK))then--updates fifo at falling edge so it can be read in rising_edge
 			fifo_sda <= fifo_sda(N downto 0) & '1';--MSB is sent first
-			SDA <= fifo_sda(N);
 			bits_sent <= bits_sent + 1;
-			if (bits_sent = N+2) then
+			if (bits_sent = N+1) then
 				fifo_empty <= '1';
 				bits_sent <= 0;
 			else
@@ -101,6 +113,7 @@ begin
 		end if;
 
 	end process;
+	SDA <= fifo_sda(N+1);
 	
 --	serial_r: process()
 	
