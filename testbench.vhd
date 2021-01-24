9a0dce550a73611ef249e520188902c15480d10e
RenanPicoli <renanps123@gmail.com>
2021-01-23 23:25:53 -0300

    added I2C_EN and WREN to testbench (driven by process setup); corrected delays for slave ack

--- testbench.vhd
+++ testbench.vhd
@@ -22,6 +22,7 @@ signal ADDR: std_logic_vector(1 downto 0);--address offset of registers relative
 signal RST:	std_logic;--reset
 signal WREN: std_logic;--enables register write
 signal RDEN: std_logic;--enables register read
+signal I2C_EN: std_logic;--enables trasfer to start
 signal IACK: std_logic;--interrupt acknowledgement
 signal Q: std_logic_vector(31 downto 0);--for register read
 signal IRQ: std_logic;--interrupt request
@@ -29,11 +30,12 @@ signal SDA: std_logic;--open drain data line
 signal SCL: std_logic;--open drain clock line
 
 signal read_mode: std_logic;
+signal write_mode: std_logic;
 begin
 	--all these times are relative to the beginning of simulation
 	--'H' models the pull up resistor in SDA line
-	SDA <= 'H','0' after 65 us,'H' after 72.5 us,
-				'0' after 115 us, 'H' after 122.5 us, '0' after 165 us, 'H' after 172.5 us;--slave ack for writes
+	SDA <= 'H','0' after 60 us,'H' after 67.5 us,
+				'0' after 110 us, 'H' after 117.5 us, '0' after 160 us, 'H' after 167.5 us;--slave ack for writes
 	
 	DUT: entity work.i2c_master
 	port map(D 		=> D,
@@ -42,6 +44,7 @@ begin
 				RST	=>	RST,
 				WREN	=> WREN,
 				RDEN	=>	RDEN,
+				I2C_EN=> I2C_EN,
 				IACK	=> IACK,
 				Q		=>	Q,
 				IRQ	=>	IRQ,
@@ -74,24 +77,27 @@ begin
 	begin
 		--zeroes & WORDS & SLV ADDR & R/W(1 read mode; 0 write mode)
 		D <= (31 downto 10 =>'0') & "01" & "0000101" & '0';--WORDS: 01; ADDR: 1010
---		D <= (31 downto 10 =>'0', 9 downto 8 => "01", 7 downto 1=> "0000101", 0=> '0');--WORDS: 01; ADDR: 1010
---		D<=(others=>'0');
+		read_mode <= D(0);
+		write_mode <= not read_mode;
 		ADDR <= "00";--CR address
+		WREN <= '1';
+		I2C_EN <= '0';
 		wait for TIME_RST + 5 us;
 		
 		--bits 7:0 data to be transmitted
 		D <= x"0000_0095";-- 1001 0101
-		ADDR <= "01";--DR address
+		ADDR <= "01";--DR address		
+		WREN <= '1';
 		wait for 5 us;
 
 		D<=(others=>'0');
 		ADDR<="11";--invalid address
-		WREN <= '1';
+		WREN <= '0';
+		I2C_EN <= '1';
 		wait for 5 us;
 		
-		WREN <= '0';
+		I2C_EN <= '0';
 		wait;
---		read_mode <= D(0);
 	end process setup;
 	RST <= '1', '0' after TIME_RST;
 	
